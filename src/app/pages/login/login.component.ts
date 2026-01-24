c24bd6ae6f15c4a70646c6e2bca0c08dabf1020c
mitrasingh <mitra.coder@gmail.com>
2026-01-24 16:33:25 -0500

    feat: add forms module, reactive forms structure

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  loginObj: any = {
-    username: '',
-    password: '',
-  };
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      alert(`Login successful, welcome + ${this.loginForm.value.username}`);
+    }
+  }
 }
