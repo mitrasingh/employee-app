daeb15fcdff63a97b28c068f12da0be32786a47f
mitrasingh <mitra.coder@gmail.com>
2026-01-23 21:40:57 -0500

    feat: add layout component with children routes of dashboard and employee component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +17,15 @@ export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'employee',
+        component: EmployeeComponent,
+      },
+    ],
   },
 ];
