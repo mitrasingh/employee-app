16cf6a5eeda1a2c7836127dc07c36c79434e592e
mitrasingh <mitra.coder@gmail.com>
2026-01-25 15:23:52 -0500

    feat: create service file for authentication

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { LoginRequest, LoginResponse } from '../models/login.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'https://dummyjson.com/auth/login';
+  private http = inject(HttpClient);
+
+  login(credentials: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(this.apiUrl, credentials);
+  }
+}
