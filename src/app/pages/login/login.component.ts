c99fe0032250ab2b7d25b5da90c4be0bf8741780
mitrasingh <mitra.coder@gmail.com>
2026-01-25 14:15:04 -0500

    feat: complete onSubmit logic and add necessary imports

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +10,30 @@ import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  http = inject(HttpClient);
+  router = inject(Router);
+  apiUrl = 'https://dummyjson.com/auth/login';
+
   loginForm = new FormGroup({
     username: new FormControl(''),
     password: new FormControl(''),
   });
 
   onSubmit() {
-    if (this.loginForm.valid) {
-      alert(`Login successful, welcome + ${this.loginForm.value.username}`);
-    }
+    this.http.post(this.apiUrl, this.loginForm.value).subscribe({
+      next: (res: any) => {
+        if (res.accessToken) {
+          localStorage.setItem('employee-management', JSON.stringify(res));
+          this.router.navigateByUrl('dashboard');
+          console.log('Login Successful');
+        } else {
+          alert('Invalid Credentials');
+        }
+      },
+      error: (error) => {
+        console.error('Login failed:', error);
+        alert('Invalid username or password');
+      },
+    });
   }
 }
