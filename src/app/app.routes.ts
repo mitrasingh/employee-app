0ce702dccf9a477c0534b5027dfc16a859cca866
mitrasingh <mitra.coder@gmail.com>
2026-01-26 19:58:36 -0500

    fix: update import source path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { LayoutComponent } from './pages/layout/layout.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { EmployeeComponent } from './pages/employee/employee.component';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EmployeeComponent } from './components/employee/employee.component';
 
 export const routes: Routes = [
   {
