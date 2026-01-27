12378cdc278d16a3c6f9383cdcb432e51c12f0d9
mitrasingh <mitra.coder@gmail.com>
2026-01-26 20:02:38 -0500

    style: adjust component to take up height of viewport

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
