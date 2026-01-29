1f8623a969a74c3991b7ccd297e372c86baada42
mitrasingh <mitra.coder@gmail.com>
2026-01-28 19:44:58 -0500

    refactor: update employee data type

--- src/app/models/employee.model.ts
+++ src/app/models/employee.model.ts
@@ -1,11 +1,14 @@
 export interface Employee {
-  employeeId: number;
-  employeeName: string;
-  contactNo: string;
-  emailId: string;
-  deptId: number;
-  password: string;
-  gender: string;
-  role: string;
-  createdDate: Date;
+  id: number;
+  employee_id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  department: string;
+  employment_type: string;
+  status: string;
+  salary: number;
+  hire_date: string;
+  office_location: string;
 }
