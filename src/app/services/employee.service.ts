45cfa56d62ad4be4e7d35105dca929d78def91b3
mitrasingh <mitra.coder@gmail.com>
2026-02-11 12:42:35 -0500

    feat: add method to get employee data

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,4 +11,8 @@ export class EmployeeService {
   constructor(private http: HttpClient) {}
 
   private baseUrl = `${environment.supabase.apiUrl}/rest/v1`;
+
+  getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.baseUrl}/employees`);
+  }
 }
