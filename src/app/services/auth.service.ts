8b6f89e38660851fd8906c617f7e5556ec5832ef
mitrasingh <mitra.coder@gmail.com>
2026-02-11 12:14:14 -0500

    refactor: add comment for credentials

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,6 +10,8 @@ export class AuthService {
   constructor(private http: HttpClient) {}
   private apiUrl = 'https://dummyjson.com/auth/login';
 
+  /*  credentials are: username: 'emilys', password: 'emilyspass' */
+
   login(credentials: LoginRequest): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(this.apiUrl, credentials);
   }
