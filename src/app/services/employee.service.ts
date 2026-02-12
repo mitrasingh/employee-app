32a3f4409fab820df120040233486e0d07dfb89d
mitrasingh <mitra.coder@gmail.com>
2026-02-12 18:30:53 -0500

    feat: add headers for authentication, include in getEmployees method

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
@@ -9,10 +9,13 @@ import { Employee } from '../models/employee.model';
 })
 export class EmployeeService {
   constructor(private http: HttpClient) {}
-
   private baseUrl = `${environment.supabase.apiUrl}/rest/v1`;
+  private headers = new HttpHeaders({
+    apikey: environment.supabase.anonKey,
+    Authorization: `Bearer ${environment.supabase.anonKey}`,
+  });
 
   getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(`${this.baseUrl}/employees`);
+    return this.http.get<Employee[]>(`${this.baseUrl}/employees`, { headers: this.headers });
   }
 }
