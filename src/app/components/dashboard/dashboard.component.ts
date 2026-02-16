a3b6e122dfbb44046bef4e67f4d7ec5d99449c98
mitrasingh <mitra.coder@gmail.com>
2026-02-16 15:47:11 -0500

    feat: add employee count property to display

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -9,10 +9,14 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  employeeCount: number = 0;
   constructor(private employeeService: EmployeeService) {
     this.employeeService.loadEmployees();
     this.employeeService.employees$.pipe(takeUntilDestroyed()).subscribe({
-      next: (data) => console.log('Employees:', data),
+      next: (data) => {
+        console.log('Employees:', data);
+        this.employeeCount = data.length;
+      },
       error: (err) => console.error('Error fetching employees:', err),
     });
   }
