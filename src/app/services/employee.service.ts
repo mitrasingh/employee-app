2b02482dc839329ed54385b900e91d5d0af979a8
mitrasingh <mitra.coder@gmail.com>
2026-02-18 15:48:44 -0500

    refactor: convert getEmployees() to an observable

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,26 +1,30 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Employee } from '../models/employee.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  constructor(private http: HttpClient) {}
   private baseUrl = `${environment.supabase.apiUrl}/rest/v1`;
   private headers = new HttpHeaders({
     apikey: environment.supabase.anonKey,
     Authorization: `Bearer ${environment.supabase.anonKey}`,
   });
 
+  constructor(private http: HttpClient) {}
   private employeesSubject = new BehaviorSubject<Employee[]>([]);
   employees$ = this.employeesSubject.asObservable();
 
-  loadEmployees(): void {
-    this.http
-      .get<Employee[]>(`${this.baseUrl}/employees`, { headers: this.headers })
-      .subscribe((data) => this.employeesSubject.next(data));
+  // loadEmployees(): void {
+  //   this.http
+  //     .get<Employee[]>(`${this.baseUrl}/employees`, { headers: this.headers })
+  //     .subscribe((data) => this.employeesSubject.next(data));
+  // }
+
+  getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.baseUrl}/employees`, { headers: this.headers });
   }
 }
