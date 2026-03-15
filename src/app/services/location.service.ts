92f3cd85ce990d005ce66c4b5f2374f73466a53d
mitrasingh <mitra.coder@gmail.com>
2026-03-15 13:28:14 -0400

    feat: create department service to fetch data from api

---
+++ src/app/services/location.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+import { Location } from '../models/location.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocationService {
+  private baseUrl = `${environment.apiUrl}/locations`;
+
+  private locationsSubject = new BehaviorSubject<Location[]>([]);
+  locations$ = this.locationsSubject.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  loadLocations(): void {
+    this.http.get<Location[]>(this.baseUrl).subscribe({
+      next: (data) => {
+        this.locationsSubject.next(data);
+      },
+      error: (err) => {
+        console.error('API error:', err);
+      },
+    });
+  }
+
+  addLocation(location: Partial<Location>) {
+    return this.http.post<Location>(this.baseUrl, location);
+  }
+
+  updateLocation(id: number, location: Partial<Location>) {
+    return this.http.put<Location>(`${this.baseUrl}/${id}`, location);
+  }
+
+  deleteLocation(id: number) {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+}
