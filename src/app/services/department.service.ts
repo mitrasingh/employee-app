e2b23b9bef02079ff151ed0fc39541ac09afc5d1
mitrasingh <mitra.coder@gmail.com>
2026-03-15 13:27:34 -0400

    feat: create department service to fetch data from api

---
+++ src/app/services/department.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+import { Department } from '../models/department.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DepartmentService {
+  private baseUrl = `${environment.apiUrl}/departments`;
+
+  private departmentsSubject = new BehaviorSubject<Department[]>([]);
+  departments$ = this.departmentsSubject.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  loadDepartments(): void {
+    this.http.get<Department[]>(this.baseUrl).subscribe({
+      next: (data) => {
+        this.departmentsSubject.next(data);
+      },
+      error: (err) => {
+        console.error('API error:', err);
+      },
+    });
+  }
+
+  addDepartment(department: Partial<Department>) {
+    return this.http.post<Department>(this.baseUrl, department);
+  }
+
+  updateDepartment(id: number, department: Partial<Department>) {
+    return this.http.put<Department>(`${this.baseUrl}/${id}`, department);
+  }
+
+  deleteDepartment(id: number) {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+}
