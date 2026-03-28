99be7f13ce0fd77f30fff8f96dc06c1bc67f37c5
mitrasingh <mitra.coder@gmail.com>
2026-03-28 08:41:31 -0400

    refactor: add constructor to load employees on initialization

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, filter } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Employee } from '../models/employee.model';
 
 @Injectable({
@@ -10,31 +11,34 @@ import { Employee } from '../models/employee.model';
 export class EmployeeService {
   private baseUrl = `${environment.apiUrl}/employees`;
 
-  private employeesSubject = new BehaviorSubject<Employee[]>([]);
-  employees$ = this.employeesSubject.asObservable();
+  private employeesSubject = new BehaviorSubject<Employee[] | null>(null);
 
-  constructor(private http: HttpClient) {}
+  employees$ = this.employeesSubject.pipe(
+    filter((employees): employees is Employee[] => employees !== null),
+  );
+
+  constructor(private http: HttpClient) {
+    this.loadEmployees();
+  }
 
   loadEmployees(): void {
     this.http.get<Employee[]>(this.baseUrl).subscribe({
-      next: (data) => {
-        this.employeesSubject.next(data);
-      },
-      error: (err) => {
-        console.error('API error:', err);
-      },
+      next: (data) => this.employeesSubject.next(data),
+      error: (err) => console.error('API error:', err),
     });
   }
 
   addEmployee(employee: Partial<Employee>) {
-    return this.http.post<Employee>(this.baseUrl, employee);
+    return this.http.post<Employee>(this.baseUrl, employee).pipe(tap(() => this.loadEmployees()));
   }
 
   updateEmployee(id: number, employee: Partial<Employee>) {
-    return this.http.put<Employee>(`${this.baseUrl}/${id}`, employee);
+    return this.http
+      .put<Employee>(`${this.baseUrl}/${id}`, employee)
+      .pipe(tap(() => this.loadEmployees()));
   }
 
   deleteEmployee(id: number) {
-    return this.http.delete(`${this.baseUrl}/${id}`);
+    return this.http.delete(`${this.baseUrl}/${id}`).pipe(tap(() => this.loadEmployees()));
   }
 }
